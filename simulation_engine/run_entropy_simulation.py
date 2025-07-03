8e9006aea46f9d50be252c916bb4fbb4faf1ee4d
tmaringgele <tmaringgele@outlook.at>
2025-07-03 08:42:55 +0200

    runtime comparison

--- simulation_engine/run_entropy_simulation.py
+++ simulation_engine/run_entropy_simulation.py
@@ -65,6 +65,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run simulations.")
     parser.add_argument("N_simulations", type=int, help="Number of simulations to run")
     parser.add_argument("--R_path", type=str, default="D:/Program Files/R/R-4.3.1", help="Path to R installation")
+    # screen -S simulation
+    # wieder verbinden: screen -r simulation
     ## Example usage: python .\run_entropy_simulation.py 2 --R_path "D:/Program Files/R-4.5.0"
     ## Example usage: python .\run_entropy_simulation.py 2 --R_path "/usr/lib/R"
 
